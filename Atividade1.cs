95164a2d0112ebf3eee392e72709e0fb37dbb738
Mariana Terra <1437020@sga.pucminas.br>
2023-04-13 00:24:45 -0300

    Três serviços

---
+++ Atividade1.cs
@@ -0,0 +1,10 @@
+﻿var servicoCEP = new ServicoCEP.AtendeClienteClient();
+var cep = "31310570";
+var result = await servicoCEP.consultaCEPAsync(cep);
+Console.WriteLine("O CEP " + cep + " é da Cidade de: " + result.@return.cidade + "\n");
+
+var result2 = await servicoCEP.buscaDataAtualAsync();
+Console.WriteLine("A data atual é: " + result2.@return + "\n");
+
+var result3 = await servicoCEP.obterMensagemParametrizadaAsync(1);
+Console.WriteLine("Mensagem Padrão: " + result3.@return.mensagem);
--- Program.cs
+++
@@ -1,3 +0,0 @@
-﻿var servicoCEP = new ServicoCEP.AtendeClienteClient();
-var result = await servicoCEP.consultaCEPAsync("31310570");
-Console.WriteLine(result.@return.cidade);
